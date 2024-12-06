b79bc3582c1f08c1a0add5a501a90e62e7900b6a
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-06 17:58:34 -0600

    feat: flags

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
 int main()
 {
-  const int HEIGHT = 20;
-  const int WIDTH = 20;
+  glfwInit();
 
-  for (int y = 0; y < HEIGHT; y++)
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+  std::cout << extensionCount << " extensions supported\n";
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
+
+  while (!glfwWindowShouldClose(window))
   {
-    for (int x = 0; x < WIDTH; x++)
-    {
-      float normalizedX = (float)x / (WIDTH - 1) - 0.5f;
-      float normalizedY = (float)y / (HEIGHT - 1) - 0.5f;
-
-      if (normalizedX * normalizedX + normalizedY * normalizedY < 0.5f * 0.5f)
-      {
-        std::cout << "  ";
-        continue;
-      }
-      std::cout << "██";
-    }
-    std::cout << std::endl;
+    glfwPollEvents();
   }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+
+  return 0;
 }
