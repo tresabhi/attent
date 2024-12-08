e04eb6e45844167f907e1f88ac74371fd99d223f
tresabhi <43380238+tresabhi@users.noreply.github.com>
2024-12-07 21:03:38 -0600

    feat: comments because i barely understand this

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <stdexcept>
 #include <vector>
 
-class HelloTriangleApplication {
+class Attent {
   public:
   void run() {
     initWindow();
@@ -26,18 +26,22 @@ class HelloTriangleApplication {
   VkInstance instance;
 
   void createInstance() {
+    // application info; not required but can help the gpu automatically
+    // optimize thing or something. sType arbitrary discriminator
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.pApplicationName = "Attent";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
+    // this one the other hand is required and leverages the stuff from above
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
+    // finding what extensions glfw needs within vulkan
     uint32_t glfwExtensionCount = 0;
     const char **glfwExtensions;
 
@@ -47,15 +51,19 @@ class HelloTriangleApplication {
     createInfo.ppEnabledExtensionNames = glfwExtensions;
     createInfo.enabledLayerCount = 0;
 
+    // creating the instance for real now
     VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
 
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-      throw std::runtime_error("failed to create instance!");
+      throw std::runtime_error("failed to create vulkan instance");
     }
 
+    // getting the extensions is a pain in the butt as we have to first figure
+    // out how many there are and then allocate an array to hold them
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
+    // now we allocate and fill
     std::vector<VkExtensionProperties> extensions(extensionCount);
     vkEnumerateInstanceExtensionProperties(
         nullptr, &extensionCount, extensions.data()
@@ -74,7 +82,7 @@ class HelloTriangleApplication {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Attent", nullptr, nullptr);
   }
 
   void initVulkan() { createInstance(); }
@@ -95,7 +103,7 @@ class HelloTriangleApplication {
 };
 
 int main() {
-  HelloTriangleApplication app;
+  Attent app;
 
   try {
     app.run();
